ab3add55725a89a0498eb5e659a361591e423567
Dmitry <146066734+DmitryTamlin7@users.noreply.github.com>
2025-04-17 18:09:32 +0300

    add ci-cd and tests

---
+++ src/test/java/org/example/dvdrental/HomeControllerTest.java
@@ -0,0 +1,24 @@
+package org.example.dvdrental;
+import org.example.controller.HomeController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(HomeController.class)
+public class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testHomePage() throws Exception {
+        mockMvc.perform(get("/home"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"));
+    }
+}
